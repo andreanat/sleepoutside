d634055f205c5620e46e6457851268506641eff4
andreanat <andrea.nmb13@gmail.com>
2025-11-05 10:11:46 -0700

    aa

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,15 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   // get current cart or start with empty array
-  const cart = getLocalStorage('so-cart') ?? [];
+  const cart = getLocalStorage("so-cart") ?? [];
   // add new product
   cart.push(product);
   // save updated cart
-  setLocalStorage('so-cart', cart);
+  setLocalStorage("so-cart", cart);
 }
 
 async function addToCartHandler(e) {
@@ -18,5 +18,5 @@ async function addToCartHandler(e) {
 }
 
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
